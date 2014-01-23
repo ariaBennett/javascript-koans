1c2edeed17c487bd3a1f5f3d77f6a4bc1c73170b
Aria Bennett <twoplustwo@gmail.com>
2014-01-23 13:03:52 -0800

    Commented out extra credit, updated a problem that was now wrong due to the new year.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -121,6 +121,7 @@ describe("About Applying What We Have Learnt", function() {
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
+  /*
   it("should find the largest prime factor of a composite number", function () {
     var isPrime = function(number) {
       for (var i = 2; i < number; i++){
@@ -252,4 +253,5 @@ describe("About Applying What We Have Learnt", function() {
   it("should find the 10001st prime", function () {
 
   });
+  */
 });
